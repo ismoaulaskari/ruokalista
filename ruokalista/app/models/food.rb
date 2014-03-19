7d9d9c79f4628cea43fc54a7f9105abb27982461
ismoaulaskari <ismo@aulaskari.com>
2014-03-19 22:44:57 +0200

    Select works finally

--- ruokalista/app/models/food.rb
+++ ruokalista/app/models/food.rb
@@ -1,4 +1,6 @@
 class Food < ActiveRecord::Base
-  validates_presence_of :week, :name, :meatiness 
+  validates_presence_of :week, :name 
+  validates_presence_of :meatiness 
+  #validates_inclusion_of :meatiness, :in => %w(vege, fish, pale, red)
   validates_inclusion_of :week, :in => 1..52
 end
