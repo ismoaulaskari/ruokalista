d031328dc9f39dafe725c53bce220fcd9a0e456f
ismoaulaskari <ismo@aulaskari.com>
2014-03-20 23:38:29 +0200

    final stuff

--- ruokalista/app/controllers/application_controller.rb
+++ ruokalista/app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
 # The logged_in? method simply returns true if the user is logged # in and false otherwise. It does this by "booleanizing" the # current_user method we created previously using a double ! operator. # Note that this is not common in Ruby and is discouraged unless you # really mean to convert something into true or false. 
 
   def logged_in? 
-    request.env['HTTP_REFERER'].start_with? 'http://artannika.com' or request.env['HTTP_REFERER'].start_with? 'http://localhost:3001'
+    request.env['HTTP_X_FORWARDED_HOST'].start_with? 'artannika.com' or request.env['HTTP_REFERER'].start_with? 'http://localhost:3001'
   end
 
   private 
