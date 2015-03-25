4e7ecc0f4f58e3deeb258dcb4c5707f7de68e760
ismoaulaskari <ismo@aulaskari.com>
2015-03-25 20:47:47 +0200

    Ruokalistaan uusia featureita: oma viikonloppuruokalista ja needbeer-kategoria

--- ruokalista/app/models/food.rb
+++ ruokalista/app/models/food.rb
@@ -2,5 +2,5 @@ class Food < ActiveRecord::Base
   validates_presence_of :week, :name 
   validates_presence_of :meatiness 
   #validates_inclusion_of :meatiness, :in => %w(vege, fish, pale, red)
-  validates_inclusion_of :week, :in => 1..52
+  validates_inclusion_of :week, :in => 0..52
 end
